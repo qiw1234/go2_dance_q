0097471a399580eabc86d850c712479a9c6edea1
pcpc <pcpc@123.com>
2025-02-28 09:12:06 +0800

    修改机身位置奖励，解决会移动的问题

--- legged_gym/legged_gym/envs/panda7/panda7_config.py
+++ legged_gym/legged_gym/envs/panda7/panda7_config.py
@@ -591,7 +591,7 @@ class panda7FixedGripperArmLegCfg(panda7FixedGripperCfg):
             # 模仿奖励
             tracking_lin_vel = 0
             tracking_ang_vel = 0
-            track_root_pos = 1.
+            track_root_pos = 5.
             track_root_height = 0.
             track_root_rot = 5
             orientation = 0
