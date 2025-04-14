ace10d858dc39b93e69859ee1606604b85462dc6
pcpc <pcpc@123.com>
2025-04-14 13:08:15 +0800

    trot频率1/0.7Hz训练结果迁移

--- sim2sim/BJ_Raisim/net/HSW/bjtu_dance_twodogs_new_actions.py
+++ sim2sim/BJ_Raisim/net/HSW/bjtu_dance_twodogs_new_actions.py
@@ -24,7 +24,7 @@ model_path_test1 = './model/test/arm_leg_2025-02-27_21-05-49.jit'
 # model 2: wave
 model_path_test2 = './model/go2/wave_2025-03-25_08-37-13.jit'  #  input 60 pd150 单臂挥舞 挥舞幅度大
 # model 3: trot
-model_path_test3 = './model/go2/trot_2025-04-10_11-38-25.jit'
+model_path_test3 = './model/go2/trot_2025-04-14_12-58-09.jit'
 # model 4: swing
 # model_path_test4 = './model/go2/swing_2025-03-24_08-54-00.jit'
 model_path_test4 = './model/go2/swing_2025-03-27_09-51-43.jit'
